d725b911013562fe55360855316ca8bccf662e88
BuildTools <unconfigured@null.spigotmc.org>
2020-01-17 17:04:06 -0600

    Gets basic ripper working
    The base ripper is working. When run, discid is determined,
    metadata grabbed from musicbrainz, disc is ripped to wav files,
    wav files converted to flac, source files deleted.
    
    v0.0.2

--- cdRipper/getMetaData.py
+++ cdRipper/getMetaData.py
@@ -4,6 +4,8 @@ try:
 except:
   logging.getLogger(__name__).critcal("Error importing 'discid', may have to set LD_LIBRARY_PATH")
 
+import os, tempfile
+from urllib.request import urlopen
 import musicbrainzngs as musicbrainz
 
 from .version import __version__, __url__
@@ -12,27 +14,28 @@ musicbrainz.set_useragent( __name__, __version__, __url__ )
   
 
 class CDMetaData( discid.Disc ):
-  def __init__(self, features = ["mnc", "isrc"] ):
+  def __init__(self, features = ["mnc", "isrc"], cache = tempfile.gettempdir() ):
     super().__init__()
     self.log      = logging.getLogger(__name__)
     self.features = features
-
+    self.cache    = cache
 
   def getMetaData(self):
     releases = self.searchMusicBrainz()
     if releases:
-      release  = self.filterReleases( releases )
-      return release
+      release = self.filterReleases( releases )
+      return self.parseRelease( release )
     return None
 
   def searchMusicBrainz(self, includes = ['artists', 'recordings', 'isrcs'] ):
-    try:
-      self.id
-    except:
-      self.log.debug('Getting information from CD')
-      self.read( features = self.features )
-    else:
-      self.log.info("Already have discID: '{}', using it again".format( self.id ) )
+   # try:
+   #   self.id
+   # except:
+   #   self.log.debug('Getting information from CD')
+   #   self.read( features = self.features )
+   # else:
+   #   self.log.info("Already have discID: '{}', using it again".format( self.id ) )
+    self.read( features = self.features )
     
     self.log.debug( 'Searching for disc on musicbrainz' )
     try:
@@ -43,20 +46,47 @@ class CDMetaData( discid.Disc ):
       return None
     else:
       if ('disc' not in result):
-        print( 'No disc information returned!' )
+        self.log.warning( 'No disc information returned!' )
         return None
     return result['disc']['release-list']
 
+  def getCoverArt(self, release):
+    try:
+      imgs = musicbrainz.get_image_list( release['id'] ) 
+    except musicbrainz.ResponseError:
+      self.log.warning( "Failed to get images" )
+    else:
+      if ('images' not in imgs):
+        self.log.warning( "No image information returned!" )
+        return None 
+    for img in imgs['images']:
+      if img['front']:
+        return self._download( img['image'] )
+    return None
+
   def parseRelease( self, release ):
+    cover  = self.getCoverArt( release )
     tracks = []
-    for track in release['medium-list']['track-list']:
-      track = {'artist'               : release['artist-credit-phrase'],
-               'album'                : release['title'],
-               'title'                : track['recording']['title'],
-               'tracknumber'          : track['number'],
-               'totaltracks'          : release['medium-list']['track-count'],
-               'musicbrainz_trackid'  : track['recording']['id'],
-               'musicbrainz_artistid' :  
+    for i, track in enumerate( release['medium-list']['track-list'] ):
+      track = {'artist'                     : release['artist-credit-phrase'],
+               'albumartist'                : release['artist-credit-phrase'],
+               'album'                      : release['title'],
+               'title'                      : track['recording']['title'],
+               'tracknumber'                : int(track['number']),
+               'totaltracks'                : int(release['medium-list']['track-count']),
+               'date'                       : release['date'],
+               'asin'                       : release['asin'],
+               'isrc'                       : self.tracks[i].isrc,
+               'discid'                     : self.id,
+               'musicbrainz_trackid'        : track['recording']['id'],
+               'musicbrainz_releasetrackid' : track['id'],
+               'musicbrainz_artistid'       : '',
+               'musicbrainz_albumid'        : release['id'],
+        }
+      if cover: track['cover-art'] = cover
+      tracks.append( track )
+    return tracks
+
   def filterReleases( self, releases ):
     isrcMatches = []
     for release in releases:
@@ -81,3 +111,15 @@ class CDMetaData( discid.Disc ):
       isrcMatches.append( nMatch )                                                          # Append nMatch to list of number of track matches
     return medium_list[ isrcMatches.index( max(isrcMatches) ) ]                             # Get maximum number of track matches; get index of that value in array, return release with most track matches based on ISRC
 
+  def _download(self, URL):
+    lcl = os.path.join(self.cache, URL.split('/')[-1])
+    try:
+      img = urlopen( URL ).read()
+    except:
+      self.log.warning('Failed to download: {}'.format(URL) )
+    else:
+      with open( lcl, 'wb' ) as fid:
+        fid.write( img )
+      self.log.info( 'Cover art downloaded to: {}'.format(lcl) )
+      return lcl
+    return None
--- cdRipper/ripCD.py
+++ cdRipper/ripCD.py
@@ -1,23 +1,73 @@
 import logging
 import os, tempfile, time, hashlib
 from subprocess import Popen, DEVNULL, STDOUT
+from datetime import datetime
+
+from .getMetaData import CDMetaData
+
+meta = CDMetaData()
 
 def randomHash():
-  return hashlib.md5( str(time.time()).encode() ).hexdigest() ) 
+  return hashlib.md5( str(time.time()).encode() ).hexdigest() 
 
-def ripCD( outDir ):
+def listDir( directory ):
+  files = []
+  for item in os.listdir( directory ):
+    path = os.path.join( directory, item )
+    if os.path.isfile( path ) and path.endswith('.wav'):
+      files.append( path )
+  return sorted( files )
+
+def cleanUp( directory ):
+  for root, dirs, items in os.listdir( directory ):
+    for item in items:
+      path = os.path.join( root, item )
+      if os.path.isfile( path ):
+        os.remove( path )
+    os.rmdir( root )        
+
+def ripCD( outDirBase ):
   log = logging.getLogger(__name__)
 
-  tmpDir = os.path.join( tempfile.gettmpdir, randomHash )
+  tmpDir = os.path.join( tempfile.gettempdir(), randomHash() )
+  tmpDir = os.path.join( tempfile.gettempdir(), '05eb03c1875061edf9478fe57945c6c6')
   if not os.path.isdir( tmpDir ):
     os.makedirs( tmpDir )
 
+  meta.cache = tmpDir
+  tracks     = meta.getMetaData()
+  if not tracks:
+    return False
+
+  outDir = os.path.join(outDirBase, tracks[0]['albumartist'], tracks[0]['album']) 
 
   log.info('Starting CD rip')
+  t0   = datetime.now()
   proc = Popen( ['cdparanoia', '-Bw'], cwd = tmpDir, stdout=DEVNULL, stderr=STDOUT )
-
-
-
   log.info('Waiting for CD rip to finish')
   proc.wait()
+  log.info( 'Rip completed in: {}'.format( datetime.now() - t0 ) )
+
+  log.debug( 'Ejecting disc' )
   Popen( ['eject'] )
+
+  log.info('Converting files to FLAC and placing in: {}'.format( outDir ) )
+  if not os.path.isdir( outDir ):
+    os.makedirs( outDir )
+
+  
+  for info, inFile in zip( tracks, listDir( tmpDir ) ):
+    cmd = ['flac']
+    if ('cover-art' in info):
+      cmd.append( '--picture={}'.format( info.pop('cover-art') ) )
+    for key, val in info.items():
+      cmd.append( '--tag={}={}'.format( key, val ) )
+    outFile = '{:02d} - {}.flac'.format(info['tracknumber'], info['title'])
+    outFile = os.path.join( outDir, outFile )
+    cmd.append( '--output-name={}'.format(outFile) )
+    cmd.append( inFile )
+    proc = Popen( cmd, stdout = DEVNULL, stderr = STDOUT )
+    proc.wait()
+    cleanUp( tmpDir )
+
+  return True
